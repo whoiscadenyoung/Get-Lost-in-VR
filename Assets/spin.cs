0e1b8f8854c50f551202eea6284317b500d4f63e
Benjamin Malik <56892822+bmalik1894@users.noreply.github.com>
2022-04-12 16:24:58 -0500

    initial commit

---
+++ Assets/spin.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    bool goingup = true;
+    void Update()
+    {
+        // Gets the local scale of a game object
+        //vector3 objectScale = transform.localScale;
+        // spins it proportionally such that one rotation is 4 seconds
+        transform.Rotate(0, Time.deltaTime * 20, 0); // 360 degrees / 4 seconds / ~30 frames = 4
+        // bounces up and down
+        
+    }
+}
